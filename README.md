0fa8e1262d2be276e9e0c12ae950d1493788686b
vicentepese <sempereperisvicente@gmail.com>
2021-01-04 12:32:29 -0800

    Compute imputePipeline and add flag

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Imputation_pipeline_v2
 Second version of imputation pipeline designed by Aditya
+
+# ISSUES
+Low memory --> have to fix the impute Files
+
+# Requirements
+## Correct functioning
+1. Full path in settings.json
+
--- main.sh
+++ main.sh
@@ -14,9 +14,25 @@ PREFIX=$(jq -r '.prefix' $SETTINGS)
 REF=$(jq -r '.ref' $SETTINGS)
 
 # Intialize folders
+FILESFOLDER=$(jq -r '.folder.FILESFOLDER' $SETTINGS)
 GWAS_BY_CHR_FOLDER=$(jq -r '.folder.GWAS_BY_CHR' $SETTINGS)
 SLURM_IMPUTE_LOG=$(jq -r '.folder.SLURM_IMPUTE_LOG' $SETTINGS)
 SHAPEIT_IMPUTE_LOG=$(jq -r '.folder.SHAPEIT_IMPUTE_LOG' $SETTINGS)
-IMPUTEFILES_FOLDER=$(jq -r '.folder.SHAPEIT_IMPUTE_LOG' $SETTINGS)
 BINFILES_FOLDER=$(jq -r '.folder.BINFILES_FOLDER' $SETTINGS)
+SCRIPTS=${FILESFOLDER}scripts/
+
+# Compute pipeline
+python scripts/imputePipe.py -F $PREFIX -Ref $REF
+
+# Sleep until job done 
+USERFLAG="INIT
+    USERFLAG"
+while $(echo "$USERFLAG" | wc -l) -lt 1 
+do 
+    # Update flag
+    USERFLAG=$(squeue -u $USER)
+    sleep 2m
+done
+
+
 
