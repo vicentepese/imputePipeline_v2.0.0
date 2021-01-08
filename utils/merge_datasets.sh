ae9f24f71c1a5ec92a828f95324cde01920896dc
vicentepese <sempereperisvicente@gmail.com>
2021-01-08 09:03:49 -0800

    merge_datasets functional version

--- utils/merge_datasets.sh
+++ utils/merge_datasets.sh
@@ -6,8 +6,8 @@
 #SBATCH --account=mignot
 
 # Initialize folders / files
-FILES=/labs/mignot/LGI1_GWAS/
-MERGELIST=/labs/mignot/GPC1_LGI1/mergelist.txt
+FILES=/labs/mignot/LGI1/LGI1_GWAS/
+MERGELIST=/labs/mignot/GPC_LGI1/mergelist.txt
 
 # Initialize plink variables 
 MAF=0.05
@@ -50,7 +50,7 @@ plink --bfile ${FILES}${MERGEVAR[0]}_QC --bmerge ${FILES}${MERGEVAR[1]}_QC \
 # For loop to merge 
 LENFILES=${#MERGEVAR[@]}
 
-for IDX in $(seq 2 $LENFILES)
+for IDX in $(seq 2 21)
 do
 
     # Take CHR 
@@ -63,7 +63,7 @@ do
     # Parse duplicated variants
     echo "------------------------------------------------------------------------------"
     plink --bfile ${FILES}${CHR} --list-duplicate-vars suppress-first \
-        --allow-no-sex --out temp >> temp
+        --allow-no-sex --out temp > temp
     awk '{print $4}' temp.dupvar > DUPSNPS.txt
     rm -r temp*
 
@@ -75,9 +75,11 @@ do
         --make-bed --out ${FILES}${CHR}_QC 
 
     # Merge 
-    plink --bfile ${FILES}LGI1_GWAS --bmerge ${FILES}${MERGEVAR[1]}_QC \
+    plink --bfile ${FILES}LGI1_GWAS --bmerge ${FILES}${CHR}_QC \
         --make-bed --out ${FILES}temp 
-    cp ${FILES}temp* ${FILES}LGI1_GWAS*
+    mv ${FILES}temp.bim ${FILES}LGI1_GWAS.bim
+    mv ${FILES}temp.bed ${FILES}LGI1_GWAS.bed
+    mv ${FILES}temp.fam ${FILES}LGI1_GWAS.fam
 
     echo "------------------------------------------------------------------------------"
 done 
@@ -86,37 +88,37 @@ done
 
 ########### ALTERNATIVE #############
 
-LENFILES=${#MERGEVAR[@]}
-for i in $(seq 0 $LENFILES)
-do 
-    # Print 
-    echo "------------------------------------------------------------------------------"
-    echo "QCing "${MERGEVAR[$i]}
+# LENFILES=${#MERGEVAR[@]}
+# for i in $(seq 0 $LENFILES)
+# do 
+#     # Print 
+#     echo "------------------------------------------------------------------------------"
+#     echo "QCing "${MERGEVAR[$i]}
 
-    # Parse duplicated variants
-    plink --bfile ${FILES}${MERGEVAR[$i]} --list-duplicate-vars suppress-first \
-        --allow-no-sex --out temp >> temp
-    awk '{print $4}' temp.dupvar > DUPSNPS.txt
-    rm -r temp*
+#     # Parse duplicated variants
+#     plink --bfile ${FILES}${MERGEVAR[$i]} --list-duplicate-vars suppress-first \
+#         --allow-no-sex --out temp >> temp
+#     awk '{print $4}' temp.dupvar > DUPSNPS.txt
+#     rm -r temp*
 
-    # QC (duplicated variants and MAF)
-    plink --bfile ${FILES}${MERGEVAR[$i]} --exclude DUPSNPS.txt \
-        --allow-no-sex \
-        --maf $MAF \
-        --make-bed --out ${FILES}${MERGEVAR[$i]}_QC 
+#     # QC (duplicated variants and MAF)
+#     plink --bfile ${FILES}${MERGEVAR[$i]} --exclude DUPSNPS.txt \
+#         --allow-no-sex \
+#         --maf $MAF \
+#         --make-bed --out ${FILES}${MERGEVAR[$i]}_QC 
 
-    # Print 
-    echo "------------------------------------------------------------------------------"
+#     # Print 
+#     echo "------------------------------------------------------------------------------"
 
-done 
+# done 
 
-# Change mergelist to plink format
-awk -v FILEPATH=$FILES '{
-    $1=FILEPATH$1"_QC.bed "FILEPATH$1"_QC.bim "FILEPATH$1"_QC.fam";
-    print $1
-}' $MERGELIST > mergeList_plink.txt
+# # Change mergelist to plink format
+# awk -v FILEPATH=$FILES '{
+#     $1=FILEPATH$1"_QC.bed "FILEPATH$1"_QC.bim "FILEPATH$1"_QC.fam";
+#     print $1
+# }' $MERGELIST > mergeList_plink.txt
 
-# Full merge 
-plink --bfile ${FILES}CHR1_LGI1_QC --merge-list mergeList_plink.txt \
-    --out ${FILES}LGI1_GWAS
+# # Full merge 
+# plink --bfile ${FILES}CHR1_LGI1_QC --merge-list mergeList_plink.txt \
+#     --out ${FILES}LGI1_GWAS
 
