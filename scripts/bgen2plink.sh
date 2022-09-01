9750cf3535eb44d5d94d74b84c89aba705bfc089
vicentepese <vsempereperis@gmail.com>
2022-09-01 06:52:31 -0700

    convert bgen to PLINK format

---
+++ scripts/bgen2plink.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=BGEN2PLINK
+#SBATCH --mem-per-cpu=16000
+#SBATCH --time=8:00:00
+#SBATCH --account=mignot
+#SBATCH --array=1-22
+
+######## INITIALIZE ########
+
+# Read JSON file
+SETTINGS=$(pwd)/settings.json
+
+# Initialize variables 
+PREFIX=$(jq -r '.prefix' $SETTINGS)
+REF=$(jq -r '.ref' $SETTINGS)
+MERGE=$(jq -r '.merge' $SETTINGS)
+
+# Intialize folders
+FILESFOLDER=$(jq -r '.folder.FILESFOLDER' $SETTINGS)
+GWAS_BY_CHR_FOLDER=$(jq -r '.folder.GWAS_BY_CHR' $SETTINGS)
+SLURM_IMPUTE_LOG=$(jq -r '.folder.SLURM_IMPUTE_LOG' $SETTINGS)
+SHAPEIT_IMPUTE_LOG=$(jq -r '.folder.SHAPEIT_IMPUTE_LOG' $SETTINGS)
+BINFILES_FOLDER=$(jq -r '.folder.BIN_FOLDER' $SETTINGS)
+SCRIPTS=${FILESFOLDER}scripts/
+
+# PREFIX
+PREFIX=LGI1
+
+# Path to files / databases
+DATA_IN=${GWAS_BY_CHR_FOLDER}CHR"${SLURM_ARRAY_TASK_ID}"_${PREFIX}.bgen
+DATA_SAMPLE=${GWAS_BY_CHR_FOLDER}CHR"${SLURM_ARRAY_TASK_ID}"_${PREFIX}.sample
+
+# Out folder
+OUTFOLDER=/oak/stanford/scg/lab_mignot/LGI1/LGI1_GWAS/
+
+# Load module
+module load qctool/v2.0.1
+
+# Merge datasets
+qctool_v2.0.1 \
+-g $DATA_IN \
+-s $DATA_SAMPLE \
+-assume-chromosome "$SLURM_ARRAY_TASK_ID" \
+-compare-variants-by position,alleles \
+-incl-samples $sampleIDs \
+-threads 16 \
+-threshold 0.8 \
+-ofiletype binary_ped \
+-og ${GWAS_BY_CHR_FOLDER}CHR"$SLURM_ARRAY_TASK_ID"_${PREFIX} \
+-os ${GWAS_BY_CHR_FOLDER}CHR"$SLURM_ARRAY_TASK_ID"_${PREFIX}.sample
+
+# Modify .fam file: ISSUE -- When converting to bed, QCTOOLS ignores the IIDs in the sample files -- loses also pheno and sex
+awk 'NR>2 {print $0}' ${OUTFOLDER}CHR"$SLURM_ARRAY_TASK_ID"_${PREFIX}.sample > temp_"$SLURM_ARRAY_TASK_ID"
+awk 'FNR==NR{a[NR]=$1;next}{$1=a[FNR]}1' temp_"$SLURM_ARRAY_TASK_ID" ${OUTFOLDER}CHR"$SLURM_ARRAY_TASK_ID"_${PREFIX}.fam > fam_temp_"$SLURM_ARRAY_TASK_ID" && mv fam_temp_"$SLURM_ARRAY_TASK_ID" ${OUTFOLDER}CHR"$SLURM_ARRAY_TASK_ID"_${PREFIX}.fam
+awk 'FNR==NR{a[NR]=$2;next}{$2=a[FNR]}1' temp_"$SLURM_ARRAY_TASK_ID" ${OUTFOLDER}CHR"$SLURM_ARRAY_TASK_ID"_${PREFIX}.fam  > fam_temp_"$SLURM_ARRAY_TASK_ID" && mv fam_temp_"$SLURM_ARRAY_TASK_ID" ${OUTFOLDER}CHR"$SLURM_ARRAY_TASK_ID"_${PREFIX}.fam
+rm temp_"$SLURM_ARRAY_TASK_ID"
+rm m_temp_"$SLURM_ARRAY_TASK_ID"
