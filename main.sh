525f6fd63ff72e0e5a3f99bd1f7df676dd27ed04
vicentepese <vsempereperis@gmail.com>
2022-09-01 06:44:21 -0700

    add iterative removal of duplicated snps and genotype filtering

--- main.sh
+++ main.sh
@@ -49,22 +49,34 @@ if [ ! -f ${PREFIX}.bed ]; then
     plink --file ${PREFIX} --allow-no-sex --no-sex --no-fid --no-parents --no-pheno --make-bed --out ${PREFIX}
 fi
 
-# Create file with duplicated variants
-plink --bfile ${PREFIX} --list-duplicate-vars suppress-first \
-    --allow-no-sex --out temp > temp
-awk '{print $4}' temp.dupvar > DupSNPs.txt
-rm -r temp*
+# Create list duplicated variants by name and position
+awk 'a[$2]++{print $0}' ${PREFIX}.bim > Dup_vars_name_pos.txt
+sort -k3n Infinium_mega_sleep_62_to_68.bim | uniq -f2 -D >> Dup_vars_name_pos.txt
 
-# Remove duplicated variants 
-plink --bfile ${PREFIX} --exclude DupSNPs.txt\
-    --allow-no-sex \
+# Iteratively remove multi-allelic variants (PLINK does not manage them well)
+NDUPVARS=$(awk 'END{print NR}' ${FILESFOLDER}Dup_vars_name_pos.txt)
+while [ $NDUPVARS -gt 1 ];
+do 
+    plink --bfile ${PREFIX} --exclude Dup_vars_name_pos.txt\
+        --allow-no-sex \
+        --make-bed --out gwastempFilt > gwastempFilt
+    rm -r ${PREFIX}.bed ${PREFIX}.fam ${PREFIX}.bim
+    mv gwastempFilt.bed ${PREFIX}.bed;  mv gwastempFilt.fam ${PREFIX}.fam;  mv gwastempFilt.bim ${PREFIX}.bim;
+    rm -r gwastempFilt*
+
+    awk 'a[$2]++{print $0}' ${PREFIX}.bim > Dup_vars_name_pos.txt
+    sort -k3n ${PREFIX}.bim | uniq -f2 -D >> Dup_vars_name_pos.txt
+    NDUPVARS=$(awk 'END{print NR}' ${FILESFOLDER}Dup_vars_name_pos.txt)
+done
+
+# Filter out low genotyping in samples and variants (Shape it throw an error at fully missing vars or subjects)
+plink --bfile ${PREFIX} --allow-no-sex \
+    --mind --geno \
     --make-bed --out gwastempFilt > gwastempFilt
-rm -r ${PREFIX}.bed ${PREFIX}.fam ${PREFIX}.bim
-mv gwastempFilt.bed ${PREFIX}.bed;  mv gwastempFilt.fam ${PREFIX}.fam;  mv gwastempFilt.bim ${PREFIX}.bim;  
-rm -r gwastempFilt*
+mv gwastempFilt.bed ${PREFIX}.bed;  mv gwastempFilt.fam ${PREFIX}.fam;  mv gwastempFilt.bim ${PREFIX}.bim;
 
-########## IMPUTE PIPELINE ###########
 
+########## IMPUTE PIPELINE ##########
 # Run pipeline
 python scripts/imputePipe.py -F $PREFIX -Ref $REF
 
