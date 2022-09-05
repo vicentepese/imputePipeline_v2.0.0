f696c03bcefd4045b9b9adf012dd130205c12dae
vicentepese <vsempereperis@gmail.com>
2022-09-05 01:26:31 -0700

    remove last line

--- scripts/bgen2plink.sh
+++ scripts/bgen2plink.sh
@@ -32,7 +32,6 @@ qctool_v2.0.1 \
 -s $DATA_SAMPLE \
 -assume-chromosome "$SLURM_ARRAY_TASK_ID" \
 -compare-variants-by position,alleles \
--incl-samples $sampleIDs \
 -threads 16 \
 -threshold 0.8 \
 -ofiletype binary_ped \
@@ -44,5 +43,4 @@ awk 'NR>2 {print $0}' ${BIN_FOLDER}CHR"$SLURM_ARRAY_TASK_ID"_${PREFIX}.sample > 
 awk 'FNR==NR{a[NR]=$1;next}{$1=a[FNR]}1' temp_"$SLURM_ARRAY_TASK_ID" ${BIN_FOLDER}CHR"$SLURM_ARRAY_TASK_ID"_${PREFIX}.fam > fam_temp_"$SLURM_ARRAY_TASK_ID" && mv fam_temp_"$SLURM_ARRAY_TASK_ID" ${BIN_FOLDER}CHR"$SLURM_ARRAY_TASK_ID"_${PREFIX}.fam
 awk 'FNR==NR{a[NR]=$2;next}{$2=a[FNR]}1' temp_"$SLURM_ARRAY_TASK_ID" ${BIN_FOLDER}CHR"$SLURM_ARRAY_TASK_ID"_${PREFIX}.fam  > fam_temp_"$SLURM_ARRAY_TASK_ID" && mv fam_temp_"$SLURM_ARRAY_TASK_ID" ${BIN_FOLDER}CHR"$SLURM_ARRAY_TASK_ID"_${PREFIX}.fam
 rm temp_"$SLURM_ARRAY_TASK_ID"
-rm fam_temp_"$SLURM_ARRAY_TASK_ID"
 
